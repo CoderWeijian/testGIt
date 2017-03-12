07984796ccb49ac61c6c3774df1ca975459e3687
MRCLO <LiaoWeijian@liaoweijiandeMac-mini.local>
2017-03-12 12:54:38 +0800

    初始化项目

---
+++ TT/TT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TT
+//
+//  Created by 廖伟健 on 2017/3/12.
+//  Copyright © 2017年 lwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TT/TT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TT
+//
+//  Created by 廖伟健 on 2017/3/12.
+//  Copyright © 2017年 lwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
